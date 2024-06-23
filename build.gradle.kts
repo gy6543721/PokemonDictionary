a842ac59927b0dc6825b82d200aad004007cc125
gy6543721 <gy6543721@yahoo.com.tw>
2024-06-23 17:49:15 +0900

    [Update] Version Catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,13 @@ buildscript {
         google()
         mavenCentral()
     }
-    dependencies {
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.51")
-        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
-    }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    alias(libs.plugins.agp) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.hilt.android.plugin) apply false
+    alias(libs.plugins.kotlin) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.secrets.gradle.plugin) apply false
 }
