7938207ef5f6ff02de0ccab0fa8e6b085f9d087d
gy6543721 <gy6543721@yahoo.com.tw>
2025-03-09 11:19:14 +0900

    [Release] 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "levilin.pokemon.dictionary"
         minSdk = 30
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
